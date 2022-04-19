d1ed37b54cc14f7862e9955377794ff787c1ae01
Zsolt Szalma <zsolt.szalma@gmail.com>
2022-04-19 12:52:11 +0200

    Refactor ejs templates

--- middlewares/logoutUserMW.js
+++ middlewares/logoutUserMW.js
@@ -2,6 +2,7 @@ const logoutUserMW = () => {
 
   return (req, res, next) => {
     req.session.userId = null;
+    res.locals.loggedInUser = null;
     next();
   };
 
--- middlewares/renderMW.js
+++ middlewares/renderMW.js
@@ -1,5 +1,8 @@
 const renderMW = (view) => {
   return (req, res) => {
+    if (typeof req.session.userId === 'undefined') {
+      res.locals.loggedInUser = null;
+    }
     res.status(200).render(view, res.locals)
   };
 };
